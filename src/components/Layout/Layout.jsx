3139db5294cedb154c9064e3c4dfcfb0e4e82f77
Prajwal B R <praj@pixel8.io>
2022-03-05 20:57:53 +0530

    Updated sidebar to latest design

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className="bg-black text-white px-16 h-screen">
       <Header />
       <div className="relative flex h-sidebarHeight">
-          <Sidebar />
-          <div className="w-full overflow-x-auto pl-8">{children}</div>
+        <Sidebar />
+        <div className="w-full overflow-x-auto pl-8">{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as AngleRight } from "../../assets/img/angle-right.svg";
+import { ReactComponent as Wallet } from "../../assets/img/wallet.svg";
+import { ReactComponent as Trophy } from "../../assets/img/trophy.svg";
+import { ReactComponent as UserGroup } from "../../assets/img/user-group.svg";
+import { ReactComponent as BullseyeArrow } from "../../assets/img/bullseye-arrow.svg";
 
 const Sidebar = () => {
   return (
-    <div className="h-full w-sidebarWidth bg-white text-black rounded-xl py-5 pl-6 pr-2 space-y-4">
-      <div className="">NFT Tools</div>
-      <div className="h-sidebarListHeight overflow-y-auto space-y-3">
-        <Link to="/" className="flex items-center space-x-2">
-          <AngleRight />
+    <div className="h-fit w-sidebarWidth bg-zinc-800 rounded-xl py-5 space-y-4">
+      <div className=" pl-6 pr-2">NFT Tools</div>
+      <div className="max-h-sidebarListHeight overflow-y-auto space-y-3">
+        <Link
+          to="/"
+          className="flex items-center py-3 pl-6 pr-2 space-x-2 hover:bg-white hover:text-black"
+        >
+          <Wallet />
           <div className="">Wallet Activity</div>
+        </Link>
+        <Link
+          to="/"
+          className="flex items-center py-3 pl-6 pr-2 space-x-2 hover:bg-white hover:text-black"
+        >
+          <Trophy />
+          <div className="">Flex</div>
+        </Link>
+        <Link
+          to="/"
+          className="flex items-center py-3 pl-6 pr-2 space-x-2 hover:bg-white hover:text-black"
+        >
+          <UserGroup />
+          <div className="">Fans</div>
+        </Link>
+        <Link
+          to="/"
+          className="flex items-center py-3 pl-6 pr-2 space-x-2 hover:bg-white hover:text-black"
+        >
+          <BullseyeArrow />
+          <div className="">Air Drop</div>
         </Link>
       </div>
     </div>
