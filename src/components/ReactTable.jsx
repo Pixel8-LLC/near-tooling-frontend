f00d634c84236b099a8ef364256775d8c8f22004
Prajwal B R <praj@pixel8.io>
2022-03-07 21:10:38 +0530

    Finished fans screen

---
+++ src/components/ReactTable.jsx
@@ -0,0 +1,73 @@
+import { useTable } from "react-table/";
+
+const ReactTable = ({ columns, data }) => {
+  const tableInstance = useTable({ columns, data });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    // apply the table props
+    <table {...getTableProps()} className="w-full">
+      <thead>
+        {
+          // Loop over the header rows
+          headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                // Loop over the headers in each row
+                headerGroup.headers.map((column) => (
+                  // Apply the header cell props
+                  <th
+                    {...column.getHeaderProps()}
+                    className="text-left py-4 border-b"
+                  >
+                    {
+                      // Render the header
+                      column.render("Header")
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {
+          // Loop over the table rows
+          rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row);
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {
+                  // Loop over the rows cells
+                  row.cells.map((cell) => {
+                    // Apply the cell props
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        className="py-4 border-b border-white/50"
+                      >
+                        {
+                          // Render the cell contents
+                          cell.render("Cell")
+                        }
+                      </td>
+                    );
+                  })
+                }
+              </tr>
+            );
+          })
+        }
+      </tbody>
+    </table>
+  );
+};
+
+export default ReactTable;
--- src/pages/Fans.jsx
+++ src/pages/Fans.jsx
@@ -1,16 +1,132 @@
-import React, { useState } from "react";
+import { useMemo, useState } from "react";
 import { ReactComponent as AngleRight } from "../assets/img/angle-right.svg";
 import { ReactComponent as FileExport } from "../assets/img/file-export.svg";
+import { ReactComponent as Eye } from "../assets/img/eye.svg";
+import clipboard from "../assets/img/clipboard.svg";
+import link from "../assets/img/link.svg";
+import { ReactComponent as RedTimes } from "../assets/img/red-times.svg";
+import { ReactComponent as GreenCheck } from "../assets/img/green-check.svg";
+import { ReactComponent as GreyClock } from "../assets/img/grey-clock.svg";
+import ReactTable from "../components/ReactTable";
 
 const Fans = () => {
   const [contractAddress, setContractAddress] = useState("");
-
+  const statusIcon = useMemo(
+    () => ({
+      Succeeded: <GreenCheck />,
+      Failed: <RedTimes />,
+      Pending: <GreyClock />,
+    }),
+    []
+  );
+  const data = useMemo(
+    () => [
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 2,
+        type: "Transfer",
+        explorer_link: "",
+        status: "Succeeded",
+      },
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 2,
+        type: "Transfer",
+        explorer_link: "",
+        status: "Failed",
+      },
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 1,
+        type: "Mint",
+        explorer_link: "",
+        status: "Pending",
+      },
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 1,
+        type: "Transfer",
+        explorer_link: "",
+        status: "Succeeded",
+      },
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 0,
+        type: "Transfer",
+        explorer_link: "",
+        status: "Failed",
+      },
+      {
+        date: "2/27/22",
+        minter: "pixel8llc.near",
+        no_of_mints: 2,
+        type: "Mint",
+        explorer_link: "",
+        status: "Pending",
+      },
+    ],
+    []
+  );
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Date",
+        accessor: "date",
+      },
+      {
+        Header: "Minter",
+        accessor: "minter",
+      },
+      {
+        Header: "Number of mints",
+        accessor: "no_of_mints",
+      },
+      {
+        Header: "Type",
+        accessor: "type",
+      },
+      {
+        Header: "Explorer Link",
+        accessor: "explorer_link",
+        Cell: ({ row, value }) => (
+          <div className="flex items-center space-x-2">
+            <div className="">{statusIcon[row.original.status] || ""}</div>
+            <div className="w-16">{row.original.status || "N/A"}</div>
+            <a href="#a">
+              <i className="fas fa-link"></i>
+            </a>
+            <button>
+              <i className="fas fa-clipboard"></i>
+            </button>
+          </div>
+        ),
+      },
+      {
+        Header: () => <div className="text-right">Action</div>,
+        id: "action",
+        Cell: () => (
+          <div className="flex justify-end">
+            <button className="mr-2">
+              <Eye />
+            </button>
+          </div>
+        ),
+      },
+    ],
+    [statusIcon]
+  );
   return (
-    <div>
+    <div className="pb-10">
       <div className="text-6xl font-medium w-full pb-3">Flex</div>
-      <div className="flex items-center space-x-6 text-lg mt-6">
+      <div className="flex items-end space-x-6 text-lg mt-6">
         <div className="flex-1">
-          <div className="text-lg rounded-lg w-11/12 border flex items-center">
+          <div className="text-lg">Enter minting contract address</div>
+          <div className="mt-4 text-lg rounded-lg w-11/12 border flex items-center">
             <input
               value={contractAddress}
               onChange={(e) => setContractAddress(e.target.value)}
@@ -23,11 +139,15 @@ const Fans = () => {
           </div>
         </div>
         <div className="ml-auto">
-          <button className="bg-zinc-800 py-4 px-10 flex items-center space-x-4 rounded-md">
+          <button className="bg-zinc-800 py-4 text-base px-10 flex items-center space-x-4 rounded-md">
             <FileExport />
             <div className="">Export Fans</div>
           </button>
         </div>
+      </div>
+
+      <div className="text-xs mt-10">
+        <ReactTable data={data} columns={columns} />
       </div>
     </div>
   );
