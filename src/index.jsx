783838c1d683e25bf96dde8c9a709e2834b8df54
Prajwal B R <praj@pixel8.io>
2022-03-08 21:08:19 +0530

    removed unused imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, useRoutes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import history from "./@history";
 
 import "./index.css";
